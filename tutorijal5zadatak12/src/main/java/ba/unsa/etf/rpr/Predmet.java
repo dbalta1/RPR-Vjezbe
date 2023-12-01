7866dc4192b19095d949939d44036a68c5d1caa2
dbalta1 <dbalta1@etf.unsa.ba>
2023-12-01 12:51:18 +0100

    prvi i drugi zadatak

---
+++ tutorijal5zadatak12/src/main/java/ba/unsa/etf/rpr/Predmet.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr;
+
+public class Predmet {
+    private String naziv;
+    private String opisPredmeta;
+
+    public void setNaziv() {
+        this.naziv = naziv;
+    }
+
+    public void setopisPredmeta() {
+        this.opisPredmeta = opisPredmeta;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public String getopisPredmeta() {
+        return opisPredmeta;
+    }
+
+}
